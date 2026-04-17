02703ee0a9e45830cf8a518092981cd99bbabf61
Kenneth <lordlqft0@gmail.com>
2026-04-16 21:20:25 -0300

    Feat: Posições

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-function mostrarMensagem() {
-    const mensagem = document.getElementById("mensagem");
-    mensagem.innerText = "Eu gosto muito de você ❤️";
-}
-
-const dataInicio = new Date("2025-07-18T00:00:00");
-
-function atualizarTempo() {
-    const agora = new Date();
-    const diferenca = agora - dataInicio;
-
-    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-    const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
-    const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
-    const segundos = Math.floor((diferenca / 1000) % 60);
-
-    document.getElementById("tempo").innerText =
-        dias + " dias, " +
-        horas + " horas, " +
-        minutos + " minutos, " +
-        segundos + " segundos";
-}
-
-setInterval(atualizarTempo, 1000);
+document.addEventListener("mousemove", function(e) {
+  const x = (window.innerWidth / 2 - e.pageX) / 50;
+  const y = (window.innerHeight / 2 - e.pageY) / 50;
+  document.body.style.backgroundPosition = x + "px " + y + "px";
+});
