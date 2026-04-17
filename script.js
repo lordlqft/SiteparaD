d17119fa2a44291e8b482f6f816c509182552a88
Kenneth <lordlqft0@gmail.com>
2026-04-16 22:02:29 -0300

    Feat: JS da data

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.addEventListener("mousemove", function(e) {
-  const x = (window.innerWidth / 2 - e.pageX) / 50;
-  const y = (window.innerHeight / 2 - e.pageY) / 50;
-  document.body.style.backgroundPosition = x + "px " + y + "px";
-});
+function calcularTempo(dataInicial) {
+  const agora = new Date();
+  let diff = agora - dataInicial;
+
+  const diasTotais = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const meses = Math.floor(diasTotais / 30);
+  const semanas = Math.floor((diasTotais % 30) / 7);
+  const dias = (diasTotais % 30) % 7;
+
+  return meses + " meses, " + semanas + " semanas e " + dias + " dias";
+}
+
+const primeiraVista = new Date("2024-08-14");
+const namoro = new Date("2025-07-18");
+
+document.getElementById("data1").innerText =
+  "Desde a primeira vez: " + calcularTempo(primeiraVista);
+
+document.getElementById("data2").innerText =
+  "Desde o namoro: " + calcularTempo(namoro);
