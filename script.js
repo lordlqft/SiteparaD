0df2c08c1e5530e4549bcd017774aa7e1a89c672
Kenneth <lordlqft0@gmail.com>
2026-04-23 09:11:53 -0300

    Fix: Honeycomb

--- script.js
+++ script.js
@@ -29,32 +29,3 @@ function atualizar() {
 
 setInterval(atualizar, 1000);
 atualizar();
-
-// Carousel functionality
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-item');
-
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-    if (i === index) {
-      slide.classList.add('active');
-    }
-  });
-}
-
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
-
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
-}
-
-// Auto-slide every 3 seconds
-setInterval(nextSlide, 3000);
-
-// Initialize
-showSlide(currentSlide);
